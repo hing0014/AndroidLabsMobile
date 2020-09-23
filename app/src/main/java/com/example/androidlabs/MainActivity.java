dded0592511f7a85437693acc4b573534fe8f83e
hing0014 <71026874+hing0014@users.noreply.github.com>
2020-09-22 20:06:08 -0400

    Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -10,11 +10,13 @@ import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 /*
  * Nas - Game Org (Feb 12, 2018). How to show toast message on click button in Android Studio [Video]. Retrieved from
  * https://www.youtube.com/watch?v=gAPTK9jORuM
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button bu;
     Switch sw;
     RelativeLayout layout;
@@ -26,22 +28,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         bu = findViewById(R.id.button);
         bu.setOnClickListener(this);
-
-        layout = findViewById(R.id.layout);
-        sw = findViewById(R.id.switcher);
-        sw.setOnCheckedChangeListener(this);
-    }
-
-    @Override
-    public void onClick(View v)
-    {
         Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message) , Toast.LENGTH_LONG).show();
-    }
 
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-    {
-        //Snackbar.make(layout, getResources().getString(R.string.snack_line), Snackbar.LENGTH_LONG);
-        //snbr.setAction("Undu", click -> sw.setChecked(!sw));
+        layout = findViewById(R.id.gridlayout);
+        sw = findViewById(R.id.switcher);
+        sw.setOnCheckedChangeListener( (whatClicked, newState) -> {
+            // Toast.makeText(MainActivity.this, "Your button is:" + newState, Toast.LENGTH_LONG).show();
+            Snackbar.make(layout, getResources().getString(R.string.snack_line), Snackbar.LENGTH_LONG)
+                    .setAction("Hello class", (v) -> whatClicked.setChecked(false))
+                    .show();
     }
 }
