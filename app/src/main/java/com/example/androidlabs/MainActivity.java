445a0725eb070cd00455bf967e96a207c11494af
hing0014 <71026874+hing0014@users.noreply.github.com>
2020-09-24 17:55:29 -0400

    step 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bu.setOnClickListener(this);
 
 
-
         layout = findViewById(R.id.layout);
         Switch sw = findViewById(R.id.switcher);
         sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
