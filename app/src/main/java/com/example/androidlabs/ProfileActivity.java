450608fcaac83bc14b37a383ab44e40721e91de8
hing0014 <71026874+hing0014@users.noreply.github.com>
2020-10-04 14:56:53 -0400

    Initial Commit

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -2,13 +2,103 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText email;
+    LinearLayout layout;
+    SharedPreferences prefs = null;
+    Button bu;
+    String savedEmail;
+    EditText typeField;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton imgBu;
+    ImageView mImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        email = findViewById(R.id.email);
+        prefs = getSharedPreferences("emailFile", Context.MODE_PRIVATE);
+        savedEmail = prefs.getString("email", "");
+        typeField = email;
+        typeField.setText(savedEmail);
+
+        imgBu = findViewById(R.id.pictureButton);
+        imgBu.setOnClickListener(bt -> dispatchTakePictureIntent());
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        saveSharedPrefs(typeField.getText().toString());
+
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+    }
+
+    public void saveSharedPrefs(String stringToSave)
+    {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("email", stringToSave);
+        editor.apply();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("emailFile");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
